c3febffd4d76dfdbe925e0d845dea0daf6b52d67
knagayama <nagayama.kazushi@gmail.com>
2020-02-03 00:07:26 +0900

    Implement CreateChallenges

--- firebase.go
+++ firebase.go
@@ -3,10 +3,10 @@ package main
 import (
 	"context"
 	"encoding/csv"
-	"strconv"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"cloud.google.com/go/firestore"
 
@@ -14,6 +14,19 @@ import (
 	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
 )
+
+// Challenge holds data for a challenge.
+type Challenge struct {
+	Round           int
+	Code            int
+	Challenger      string
+	ChallengerRank  int
+	ChallengerScore int
+	Defender        string
+	DefenderRank    int
+	DefenderScore   int
+	Division        string
+}
 
 // InitTeams loads a local csv file given by path and uploads it to Firestore.
 func InitTeams(ctx context.Context, client *firestore.Client, path string) {
@@ -63,7 +76,7 @@ func InitTeams(ctx context.Context, client *firestore.Client, path string) {
 }
 
 // InitRanking initializes the ranking based on user input.
-func InitRanking(ctx context.Context, client *firestore.Client, currentRound string) error {
+func InitRanking(ctx context.Context, client *firestore.Client, currentRound int) error {
 	// Read from the teams document.
 	var rank string
 
@@ -79,7 +92,7 @@ func InitRanking(ctx context.Context, client *firestore.Client, currentRound str
 		data := doc.Data()
 		fmt.Println("Enter the ranking for: ", data["name"])
 		fmt.Scanln(&rank)
-		_, err = client.Collection("ranking").Doc(currentRound).Set(ctx, map[string]interface{}{
+		_, err = client.Collection("ranking").Doc(strconv.Itoa(currentRound)).Set(ctx, map[string]interface{}{
 			rank: data["name"],
 		}, firestore.MergeAll)
 		if err != nil {
@@ -90,12 +103,12 @@ func InitRanking(ctx context.Context, client *firestore.Client, currentRound str
 }
 
 // InputScores uploads challenge scores based on user input.
-func InputScores(ctx context.Context, client *firestore.Client, currentRound string) {
+func InputScores(ctx context.Context, client *firestore.Client, currentRound int) {
 	// Read challenges for the current round and input the scores.
 }
 
 // GenerateRanking generates ranking for the current round based on the last challenge scores.
-func GenerateRanking(ctx context.Context, client *firestore.Client, currentRound string) {
+func GenerateRanking(ctx context.Context, client *firestore.Client, currentRound int) {
 	// Read the scores from last challenge scores.
 
 	// Identify the winner and the loser for each division.
@@ -104,10 +117,10 @@ func GenerateRanking(ctx context.Context, client *firestore.Client, currentRound
 }
 
 // CreateChallenges generate challenges based on the current team ranking and uploads it to Firestore.
-func CreateChallenges(ctx context.Context, client *firestore.Client, currentRound string) {
+func CreateChallenges(ctx context.Context, client *firestore.Client, currentRound int) {
 	// Get ranking from current round.
 	teams := make(map[int]string)
-	dsnap, err := client.Collection("ranking").Doc(currentRound).Get(ctx)
+	dsnap, err := client.Collection("ranking").Doc(strconv.Itoa(currentRound)).Get(ctx)
 	if err != nil {
 		log.Fatalln("Error reading ranking from Firestore: ", err)
 	}
@@ -122,23 +135,93 @@ func CreateChallenges(ctx context.Context, client *firestore.Client, currentRoun
 		teams[i] = value.(string)
 	}
 
-	// Figure out divisions. 
+	// Figure out divisions. Assign map[string][]int which maps divisions to ranks.
 	// Division have the followings names:
 	// X, S+ Upper, S+ Lower, S Upper, S Middle, S Lower, A+ Upper, A+ Middle, A+ Lower,
 	// A Upper, A Middle, A Lower, B Upper, B Middle, B Lower.
-	// If len(teams) % 3 == 0, then all divisions have 3 teams.
-	// If len(teams) % 3 == 1, then the top division and the last division have 2 teams.
-	// If len(teams) % 3 == 2, then last division has 2 teams.
+
+	divisions := []string{"X", "S+ Upper", "S+ Lower", "S Upper", "S Middle", "S Lower",
+		"A+ Upper", "A+ Middle", "A+ Lower", "A Upper", "A Middle", "A Lower",
+		"B+ Upper", "B+ Middle", "B+ Lower", "B Upper", "B Middle", "B Lower",
+		"C+ Upper", "C+ Middle", "C+ Lower", "C Upper", "C Middle", "C Lower"}
+	divisionToTeam := make(map[string][]int)
 
 	switch len(teams) % 3 {
-	case 0:
+	// If len(teams) % 3 == 0, then all divisions have 3 teams.
+	// If len(teams) % 3 == 2, then last division has 2 teams.
+	case 0, 2:
+		for i, j := 1, 0; i < len(teams)+1; i++ {
+			divisionToTeam[divisions[j]] = append(divisionToTeam[divisions[j]], i)
+			if i%3 == 0 {
+				j++
+			}
+		}
+	// If len(teams) % 3 == 1, then the top division and the last division have 2 teams.
 	case 1:
-	case 2:
+		for i, j := 1, 0; i < len(teams)+1; i++ {
+			divisionToTeam[divisions[j]] = append(divisionToTeam[divisions[j]], i)
+			if i%3 == 2 {
+				j++
+			}
+		}
 	}
 
+	challenges := make(map[string]Challenge)
+
 	// Generate a challenge for each team within the division.
+	for division, divTeam := range divisionToTeam {
+		for key, teamRank := range divTeam {
+			fmt.Println(division, key, teamRank, teams[teamRank])
+			switch key {
+			case 0:
+				var challenge Challenge
+				challenge.Division = division
+				challenge.Round = currentRound
+				challenge.Defender = teams[teamRank]
+				challenge.DefenderRank = teamRank
+				challenge.Challenger = teams[teamRank+1]
+				challenge.ChallengerRank = teamRank + 1
+				challenge.Code = teamRank
+				challenges[strconv.Itoa(challenge.Code)] = challenge
+
+				challenge.Division = division
+				challenge.Round = currentRound
+				challenge.Defender = teams[teamRank]
+				challenge.DefenderRank = teamRank
+				challenge.Challenger = teams[teamRank+2]
+				challenge.ChallengerRank = teamRank + 2
+				challenge.Code = teamRank + 1
+				challenges[strconv.Itoa(challenge.Code)] = challenge
+
+			case 1:
+				var challenge Challenge
+				challenge.Division = division
+				challenge.Round = currentRound
+				challenge.Defender = teams[teamRank]
+				challenge.DefenderRank = teamRank
+				challenge.Challenger = teams[teamRank+1]
+				challenge.ChallengerRank = teamRank + 1
+				challenge.Code = teamRank + 1
+				challenges[strconv.Itoa(challenge.Code)] = challenge
+			}
+		}
+	}
+
+	for i := 1; i < len(teams)+1; i++ {
+		challenge := challenges[strconv.Itoa(i)]
+		code := i
+		fmt.Println("Spladder#4 Division", challenge.Division, " [", currentRound, "-", code, "]", challenge.ChallengerRank, "位", challenge.Challenger, "vs", challenge.DefenderRank, "位", challenge.Defender)
+	}
 
 	// Populate to Firestore.
+	for matchCode, challenge := range challenges {
+		_, err = client.Collection("challenges").Doc(strconv.Itoa(currentRound)).Set(ctx, map[string]interface{}{
+			matchCode: challenge,
+		}, firestore.MergeAll)
+		if err != nil {
+			log.Printf("An error has occurred: %s", err)
+		}
+	}
 }
 
 func main() {
@@ -157,7 +240,7 @@ func main() {
 	defer client.Close()
 
 	// Get current raound.
-	var currentRound string
+	var currentRound int
 	fmt.Println("Enter the current round: ")
 	fmt.Scanln(&currentRound)
 
